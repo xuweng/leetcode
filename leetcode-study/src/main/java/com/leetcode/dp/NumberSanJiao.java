29ec30aebe5d8f21e9508471a7c1b6df5cb6448a
xuweng <2028403305@qq.com>
2019-12-08 22:38:34 +0800

    数字三角形最大的路径之和

---
+++ leetcode-study/src/main/java/com/leetcode/dp/NumberSanJiao.java
@@ -0,0 +1,25 @@
+package com.leetcode.dp;
+
+/**
+ * 从顶点开始,每个数字向下层走只能有左下和右下两个方向,求出到达最后一行时最大的路径之和。
+ * <p>
+ * 数字三角形最大的路径之和
+ */
+public class NumberSanJiao {
+    /**
+     * 递归最大的路径之和
+     *
+     * @param a
+     * @param n 行数
+     * @param i 行坐标
+     * @param j 纵坐标
+     * @return
+     */
+    public static int maxSumRe(int[][] a, int n, int i, int j) {
+        if (i >= n) {
+            return 0;
+        }
+
+        return Math.max(maxSumRe(a, n, i + 1, j), maxSumRe(a, n, i + 1, j + 1)) + a[i][j];
+    }
+}
---
+++ leetcode-study/src/test/java/com/leetcode/recursive/NumberSanJiaoTest.java
@@ -0,0 +1,15 @@
+package com.leetcode.recursive;
+
+import com.leetcode.dp.NumberSanJiao;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class NumberSanJiaoTest {
+    @Test
+    public void maxSumReTest() {
+        int[][] a = {{7}, {3, 8}, {8, 1, 0}, {2, 7, 4, 4}, {4, 5, 2, 6, 5}};
+        int n = 5, i = 0, j = 0;
+
+        Assert.assertEquals(30, NumberSanJiao.maxSumRe(a, n, i, j));
+    }
+}
