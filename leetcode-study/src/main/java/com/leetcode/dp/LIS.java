b0830d74c85fd5289c11ec4e30434c1070e655cd
xuweng <2028403305@qq.com>
2019-12-09 22:07:43 +0800

    递归备忘录最长上升子序列

--- leetcode-study/src/main/java/com/leetcode/dp/LIS.java
+++ leetcode-study/src/main/java/com/leetcode/dp/LIS.java
@@ -1,10 +1,18 @@
 package com.leetcode.dp;
 
+import java.util.Arrays;
+
 /**
  * 最长上升子序列
  * d(i)就是找以A[i]结尾的lis
  */
 public class LIS {
+    //备忘录
+    private static int[] memo = new int[100];
+
+    static {
+        Arrays.fill(memo, -1);
+    }
 
     /**
      * 递归最长上升子序列
@@ -26,6 +34,33 @@ public class LIS {
             max = Math.max(max, temp);
         }
 
+        return max;
+    }
+
+    /**
+     * 递归备忘录最长上升子序列
+     *
+     * @param a
+     * @param i 数组下标
+     * @return
+     */
+    public static int lisReMemo(int a[], int i) {
+        if (memo[i] != -1) {
+            return memo[i];
+        }
+        if (i < 0) {
+            return 0;
+        }
+        if (i == 0) {
+            memo[i] = 1;
+            return memo[i];
+        }
+        int max = -1;
+        for (int j = 0; j <= i - 1; j++) {
+            int temp = a[j] > a[i] ? 1 : lisRe(a, j) + 1;
+            max = Math.max(max, temp);
+        }
+        memo[i] = max;
         return max;
     }
 
--- leetcode-study/src/test/java/com/leetcode/dp/LISTest.java
+++ leetcode-study/src/test/java/com/leetcode/dp/LISTest.java
@@ -12,6 +12,13 @@ public class LISTest {
     }
 
     @Test
+    public void lisReMemoTest() {
+//        int[] a = {2, 7, 1, 5, 6, 4, 3, 8, 9};
+        int[] a = {3, 1, 2, 6, 4, 5, 10, 7};
+        Assert.assertEquals(5, LIS.lisReMemo(a, a.length - 1));
+    }
+
+    @Test
     public void lisDpTest() {
 //        int[] a = {2, 7, 1, 5, 6, 4, 3, 8, 9};
         int[] a = {3, 1, 2, 6, 4, 5, 10, 7};
