b3d62f1cb7344a74d55ea4c34e0c39d52b2b8fe1
xuweng <2028403305@qq.com>
2019-12-11 22:32:10 +0800

    散列表和链表两种数据结构组合使用

--- leetcode-study/src/main/java/com/leetcode/lru/HashLRU.java
+++ leetcode-study/src/main/java/com/leetcode/lru/HashLRU.java
@@ -1,5 +1,7 @@
 package com.leetcode.lru;
 
+import java.util.Objects;
+
 /**
  * 散列表和链表两种数据结构组合使用
  */
@@ -8,8 +10,78 @@ public class HashLRU {
     private static Integer capacity = 100;
     //缓存大小
     private static Integer size = 0;
+    //头结点
+    private static Node<Integer> head = new Node<>(-1);
+    //尾结点
+    private static Node<Integer> tail = head;
 
-    private Node<Integer>[] table = new Node[capacity];
+    private static Node<Integer>[] table = new Node[capacity];
+
+    public static int find(Integer date) {
+        int index = hash(date);
+        Node<Integer> node = getPrefix(table[index], date);
+        if (node != null) {
+            //当找到数据之后，我们还需要将它删除,然后插入到双向链表的尾部
+
+        }
+
+        return -1;
+    }
+
+
+    /**
+     * 插入尾部
+     *
+     * @param date
+     * @return
+     */
+    private static Node<Integer> insertTail(Integer date) {
+        Node<Integer> node = new Node<>(date);
+        tail.next = node;
+        tail = node;
+
+        return node;
+    }
+
+    /**
+     * 返回date的前驱
+     *
+     * @return
+     */
+    private static Node<Integer> getPrefix(Node<Integer> node, Integer date) {
+        Objects.requireNonNull(node);
+        //没有头结点
+        Node<Integer> pre = node;
+
+        while (pre.next != null) {
+            if (date.equals(pre.next.date)) {
+                return pre;
+            }
+            pre = pre.next;
+        }
+
+        return null;
+    }
+
+    /**
+     * 返回找到的结点
+     *
+     * @param node
+     * @param date
+     * @return
+     */
+    private static Node<Integer> findNode(Node<Integer> node, Integer date) {
+        Objects.requireNonNull(node);
+
+        while (node != null) {
+            if (date.equals(node.date)) {
+                return node;
+            }
+
+            node = node.hNext;
+        }
+        return null;
+    }
 
     /**
      * 简单hash
